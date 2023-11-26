bf726b909923163c5c286c6cf0743034b2cec275
Adam Jonas <adamjonas25@gmail.com>
2023-11-26 21:11:41 +0000

    Improve code gen

--- aoc-scripts/create.sh
+++ aoc-scripts/create.sh
@@ -49,79 +49,21 @@ _get_input() {
 
 _create_files() {
   src_file="${src_path}Day${day}.java"
+  src_template="$AOC_SRC_CODE_GEN_FILE"
   if [[ ! -f "${src_file}" ]]; then
-    cat >"${src_file}" <<EOL
-package io.github.aj8gh.aoc.y${year}.d${day};
-
-import java.util.List;
-
-public class Day${day} {
-
-  public int part1(List<String> input) {
-    return 0;
-  }
-
-  public int part2(List<String> input) {
-    return 0;
-  }
-}
-EOL
+    cat "$src_template" > "${src_file}"
+    sed -i.bak "s/\${year}/${year}/" "${src_file}"
+    sed -i.bak "s/\${day}/${day}/" "${src_file}"
+    rm "${src_file}.bak"
   fi
 
   test_file="${test_path}Day${day}Test.java"
+  test_template="$AOC_TEST_CODE_GEN_FILE"
   if [[ ! -f "${test_file}" ]]; then
-    cat >"${test_file}" <<EOL
-package io.github.aj8gh.aoc.y${year}.d${day};
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import io.github.aj8gh.aoc.util.InputProvider;
-import java.util.List;
-import java.util.stream.Stream;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-class Day${day}Test extends InputProvider {
-
-  private Day${day} classUnderTest;
-
-  @BeforeEach
-  void setUp() {
-    classUnderTest = new Day${day}();
-  }
-
-  @ParameterizedTest
-  @MethodSource(value = INPUT_PROVIDER_PART_1)
-  void part1(List<String> input, int expected) {
-    var actual = classUnderTest.part1(input);
-    assertEquals(expected, actual);
-  }
-
-  @ParameterizedTest
-  @MethodSource(value = INPUT_PROVIDER_PART_2)
-  void part2(List<String> input, int expected) {
-    var actual = classUnderTest.part2(input);
-    assertEquals(expected, actual);
-  }
-
-  private static Stream<Arguments> inputProviderPart1() {
-    return getInput(0, 0);
-  }
-
-  private static Stream<Arguments> inputProviderPart2() {
-    return getInput(0, 0);
-  }
-
-  private static Stream<Arguments> getInput(int example, int result) {
-    return Stream.of(
-        Arguments.of(reader().getExample(DAY_${day}).asStringList(), example),
-        Arguments.of(reader().getInput(DAY_${day}).asStringList(), result)
-    );
-  }
-}
-EOL
+    cat "$test_template" > "${test_file}"
+    sed -i.bak "s/\${year}/${year}/" "${test_file}"
+    sed -i.bak "s/\${day}/${day}/" "${test_file}"
+    rm "${test_file}.bak"
   fi
 }
 
